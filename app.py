e6ca20089fd82abb1da7421d87f5ca1906b85f36
Nolan Hicks <ndhapple@gmail.com>
2014-05-03 14:08:19 -0500

    finished building index.html table of homicides, then added links from index to templated details page.

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+csv_path = './static/2007-homicides.csv'
+csv_obj = csv.DictReader(open(csv_path, 'r'))
+csv_list = list(csv_obj)
+csv_dict = dict([[o['HomicideNbr'], o] for o in csv_list])
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+        object_list=csv_list,
+    )
+    
+@app.route('/<number>/')
+def detail(number):
+    return render_template('detail.html',
+        object=csv_dict[number],
+    )
 
 if __name__ == '__main__':
     app.run(
