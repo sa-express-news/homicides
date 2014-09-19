460e13eed7664ad30a372490dfb54c55c1109536
Joseph Kokenge <jkokenge@gmail.com>
2014-09-19 11:02:37 -0500

    added personal google analytics code to deployed site while we wait for hearst analytics

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def page_not_found(e):
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         use_reloader=True,
         debug=True,
     )
